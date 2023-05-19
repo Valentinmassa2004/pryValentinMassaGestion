dd8fadbacce3777eecd34a25545f8ba687f956ed
root <mgiraudo@ies21.edu.ar>
2023-05-19 14:20:43 -0300

    19/05/2023

--- frmMostrar.Designer.cs
+++ frmMostrar.Designer.cs
@@ -28,55 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.cboactividad = new System.Windows.Forms.ComboBox();
+            this.lblactividad = new System.Windows.Forms.Label();
+            this.lbox = new System.Windows.Forms.ListBox();
+            this.btnvolver = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cboactividad
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(123, 28);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cboactividad.FormattingEnabled = true;
+            this.cboactividad.Location = new System.Drawing.Point(123, 28);
+            this.cboactividad.Name = "cboactividad";
+            this.cboactividad.Size = new System.Drawing.Size(121, 21);
+            this.cboactividad.TabIndex = 0;
             // 
-            // label1
+            // lblactividad
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "actividad";
+            this.lblactividad.AutoSize = true;
+            this.lblactividad.Location = new System.Drawing.Point(28, 28);
+            this.lblactividad.Name = "lblactividad";
+            this.lblactividad.Size = new System.Drawing.Size(50, 13);
+            this.lblactividad.TabIndex = 1;
+            this.lblactividad.Text = "actividad";
             // 
-            // listBox1
+            // lbox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(31, 73);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(325, 225);
-            this.listBox1.TabIndex = 2;
+            this.lbox.FormattingEnabled = true;
+            this.lbox.Location = new System.Drawing.Point(31, 73);
+            this.lbox.Name = "lbox";
+            this.lbox.Size = new System.Drawing.Size(325, 225);
+            this.lbox.TabIndex = 2;
             // 
-            // button1
+            // btnvolver
             // 
-            this.button1.Location = new System.Drawing.Point(280, 331);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "volver";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnvolver.Location = new System.Drawing.Point(280, 331);
+            this.btnvolver.Name = "btnvolver";
+            this.btnvolver.Size = new System.Drawing.Size(75, 23);
+            this.btnvolver.TabIndex = 3;
+            this.btnvolver.Text = "volver";
+            this.btnvolver.UseVisualStyleBackColor = true;
             // 
             // frmMostrar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnvolver);
+            this.Controls.Add(this.lbox);
+            this.Controls.Add(this.lblactividad);
+            this.Controls.Add(this.cboactividad);
             this.Name = "frmMostrar";
             this.Text = "frmMostrar";
             this.ResumeLayout(false);
@@ -86,9 +86,9 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboactividad;
+        private System.Windows.Forms.Label lblactividad;
+        private System.Windows.Forms.ListBox lbox;
+        private System.Windows.Forms.Button btnvolver;
     }
 }
